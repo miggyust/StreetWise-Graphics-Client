5aff51d1c2dce5252b6e82426f7dcf6e1f803218
jolouste <jaylord.ramos.iics@ust.edu.ph>
2021-10-02 20:16:14 +0800

    added draggable slider @app.js

---
+++ app.js
@@ -0,0 +1,91 @@
+const sliderNew = document.querySelector(".container--collection__newarrivals");
+const sliderDesigns = document.querySelector(".container--collection__designs");
+const sliderShirts = document.querySelector(".container--collection__shirts");   
+
+let isClicked = false;
+let startX;
+let scrollLeft;
+
+//Draggable Slider for New Arrivals
+sliderNew.addEventListener('mousedown', (e) =>{
+    isClicked = true;
+    startX = e.pageX - sliderNew.offsetLeft;
+    scrollLeft = sliderNew.scrollLeft;
+    sliderNew.style.cursor = "grabbing";
+    
+});
+
+sliderNew.addEventListener('mouseleave', () => {
+    isClicked = false;
+    sliderNew.style.cursor = "grab";
+    
+});
+
+sliderNew.addEventListener('mouseup' ,() => {
+    isClicked = false;
+    sliderNew.style.cursor = "grab";
+});
+
+sliderNew.addEventListener('mousemove', (e) =>{
+    if(!isClicked) return;
+    e.preventDefault();
+    const x = e.pageX - sliderNew.offsetLeft;
+    const scroll = (x - startX) * 1;
+    sliderNew.scrollLeft = scrollLeft - scroll;
+});
+
+//Dragabble for Designs
+sliderDesigns.addEventListener('mousedown', (e) =>{
+    isClicked = true;
+    startX = e.pageX - sliderDesigns.offsetLeft;
+    scrollLeft = sliderDesigns.scrollLeft;
+    sliderDesigns.style.cursor = "grabbing";
+    
+});
+
+sliderDesigns.addEventListener('mouseleave', () => {
+    isClicked = false;
+    sliderDesigns.style.cursor = "grab";
+    
+});
+
+sliderDesigns.addEventListener('mouseup' ,() => {
+    isClicked = false;
+    sliderDesigns.style.cursor = "grab";
+});
+
+sliderDesigns.addEventListener('mousemove', (e) =>{
+    if(!isClicked) return;
+    e.preventDefault();
+    const x = e.pageX - sliderDesigns.offsetLeft;
+    const scroll = (x - startX) * 1;
+    sliderDesigns.scrollLeft = scrollLeft - scroll;
+});
+
+////Dragabble for Shirts
+sliderShirts.addEventListener('mousedown', (e) =>{
+    isClicked = true;
+    startX = e.pageX - sliderShirts.offsetLeft;
+    scrollLeft = sliderShirts.scrollLeft;
+    sliderShirts.style.cursor = "grabbing";
+    
+});
+
+sliderShirts.addEventListener('mouseleave', () => {
+    isClicked = false;
+    sliderShirts.style.cursor = "grab";
+    
+});
+
+sliderShirts.addEventListener('mouseup' ,() => {
+    isClicked = false;
+    sliderShirts.style.cursor = "grab";
+});
+
+sliderShirts.addEventListener('mousemove', (e) =>{
+    if(!isClicked) return;
+    e.preventDefault();
+    const x = e.pageX - sliderShirts.offsetLeft;
+    const scroll = (x - startX) * 1;
+    sliderShirts.scrollLeft = scrollLeft - scroll;
+});
