56ac6611be889e94ad5c54ec897f088bdcd655e8
jolouste <jaylord.ramos.iics@ust.edu.ph>
2021-10-14 13:56:33 +0800

    modified and added hamburgermenu.js

---
+++ hamburgermenu.js
@@ -0,0 +1,9 @@
+/*
+    FUNCTION FOR OPPENING AND CLOSING THE HAMBURGER MENU.
+*/
+
+function hamburgerMenu() {
+    document.getElementById("menu").classList.toggle("alter");
+    document.getElementById("contents").classList.toggle("menuContents");
+    document.getElementById("menu-background").classList.toggle("changeBackground");
+}
