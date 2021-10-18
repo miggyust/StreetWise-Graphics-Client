1d676b2ff98d071438c68eadce336235eb0d7663
Rho Chryzler <rhochryzler.collado.iics@ust.edu.ph>
2021-10-18 12:52:06 +0800

    added database

--- database/database.js
+++ database/database.js
@@ -4,6 +4,12 @@ const mysql = require("mysql2");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.static('./public'))
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -11,13 +17,37 @@ const con = mysql.createConnection({
     database: "streetwisegraphics"
 });
 
+
 con.connect((err) => {
     if (!err){
+       
        console.log("connected"); 
     }else{
-        console.log("failed to connect to sql server");
+        console.log("failed to connect");
     }
 });
+
+app.post("/create", (req,res) => {
+    const firstname =  req.body.firstname //name from the html
+    const lastname = req.body.lastname  
+    const selectdate = req.body.selectdate 
+    const selecttime = req.body.selecttime
+    const phonenum = req.body.phonenum 
+    const mail = req.body.mail
+    const platform = req.body.platform 
+    const message = req.body.message
+
+    const queryString = "INSERT INTO test (firstname, lastname, selectdate, selecttime, phonenum, mail, platform, message) VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
+    con.query(queryString, [firstname,lastname,selectdate,selecttime,phonenum,mail,platform,message],(err, results, fields)=>{
+        if (err){
+            console.log("Failed to insert" + err)
+            res.sendStatus(500)
+            return
+        }
+        res.end("Data Saved Succesfully");
+    })
+    con.end();
+})
 
 app.get("/create-SGdb", (req, res) =>{
     let sql = "CREATE DATABASE streetwisegraphics";
@@ -25,11 +55,10 @@ app.get("/create-SGdb", (req, res) =>{
         if(!err){
             res.send("successfully created SG database");
         }else{
-            res.send("failed to create SG database")
+            res.send("failed to created SG database");
         }
     })
 })
-
 
 
 app.listen(PORT, () =>{
