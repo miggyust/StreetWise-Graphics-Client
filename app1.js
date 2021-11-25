801b41e76506ed7332455ad3e06a22b569dd84ed
Rho Chryzler <rhochryzler.collado.iics@ust.edu.ph>
2021-11-25 08:33:35 +0800

    push to heroku

---
+++ app1.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const router = require("./database/routes/customer.js")
+
+app.use(express.static('./'))
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(router)
+
+app.listen(PORT, () =>{
+    console.log("Listening to port " + PORT);
+});
