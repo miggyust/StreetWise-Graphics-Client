097d83cb8c998167e0be57fb62d08fac9949b023
Rho Chryzler <rhochryzler.collado.iics@ust.edu.ph>
2021-12-02 14:29:19 +0800

    Update app1.js

--- app1.js
+++ app1.js
@@ -3,16 +3,16 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-/*const router = require("./database/routes/customer.js")
-
+const router = require("./database/routes/customer.js")
+/*
 app.use(express.static('./'))
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-
-app.use(router)
 */
+app.use(router)
+
 
 app.get("/",(req,res)=>{
   res.send("Streetwise Graphics");
