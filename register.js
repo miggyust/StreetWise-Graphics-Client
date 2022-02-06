e997136cfd9acc8bc12e686b23f9aeb3066a6503
Rho Chryzler <rhochryzler.collado.iics@ust.edu.ph>
2022-02-06 20:13:01 +0800

    register

--- register.js
+++ register.js
@@ -1,10 +1,15 @@
 var check = function() {
+    let match = false;
     if (document.getElementById('password').value ==
       document.getElementById('confirm_Password').value) {
       document.getElementById('message').style.color = 'green';
       document.getElementById('message').innerHTML = 'matching';
+      match=true;
     } else {
       document.getElementById('message').style.color = 'red';
       document.getElementById('message').innerHTML = 'not matching';
+    }
+    if (match==false){
+        alert("Passwords do not match");
     }
   }
